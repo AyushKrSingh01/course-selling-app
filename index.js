16569e9eb3ba0f6770f3ef8cdcfc080cb148bde0
Ayush Kumar Singh <ayushrajput433@gmail.com>
2026-07-09 14:50:15 +0530

    Added MongoDB connection

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const {userRouter} = require('./routes/user');
 const {courseRouter} = require('./routes/course');
 const {adminRouter} = require('./routes/admin');
+const mongoose = require("mongoose");
+require('dotenv').config();
 const app = express();
 
 app.use('/user',userRouter);
 app.use('/course',courseRouter);
 app.use('/admin',adminRouter);
- 
-app.listen(3000);
+app.use(express.json());
+
+async function main(){
+ await mongoose.connect(process.env.MONGODB_URI);
+ app.listen(3000);
+    console.log("listing on port 3000");
+}
+
+main();
