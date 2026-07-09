fb322077d9e7364a723ce98f794a0c6587f6b3f8
Ayush Kumar Singh <ayushrajput433@gmail.com>
2026-07-09 13:30:56 +0530

    added admin router

---
+++ routes/admin.js
@@ -0,0 +1,33 @@
+const {Router} = require('express');
+const adminRouter = Router();
+adminRouter.post('/signup',(req,res)=>{
+    res.json({
+        message:"signup endpoint"
+    }) 
+})
+adminRouter.post('/signin',(req,res)=>{
+    res.json({
+        message:"login endpoint"
+    })
+})
+adminRouter.post('/course',(req,res)=>{
+    res.json({
+        message:"login endpoint"
+    })
+})
+
+adminRouter.put('/course',(req,res)=>{
+    res.json({
+        message:"login endpoint"
+    })
+})
+
+adminRouter.get('/course/bulk',(req,res)=>{
+    res.json({
+        message:"login endpoint"
+    })
+})
+
+module.exports={
+    adminRouter : adminRouter
+}
