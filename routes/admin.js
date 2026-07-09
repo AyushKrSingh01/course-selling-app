ea116af37c34f52e473a025d8a12f5c3cebac6a7
Ayush Kumar Singh <ayushrajput433@gmail.com>
2026-07-09 16:08:40 +0530

    added scripts

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,14 @@
 const {Router} = require('express');
 const adminRouter = Router();
 const {adminModel} = require('../db');
+const bcrypt = require('bcrypt')
+const zod = require('zod');
+const JWT = require('jsonwebtoken');
 adminRouter.post('/signup',(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const hashedPassword= bcrypt.hash(password);
+    
     res.json({
         message:"signup endpoint"
     }) 
